2b8b777ec0e521763789cfc42fb5eaba349ef98d
BrunoFNRodrigues <bf17669@gmail.com>
2021-11-18 17:51:54 -0300

    versão legal UwU

--- game_reviews/game_reviews/spiders/GameInformer.py
+++ game_reviews/game_reviews/spiders/GameInformer.py
@@ -11,5 +11,6 @@ class GameinformerSpider(scrapy.Spider):
             yield{
                 "titulo" : teaser.css(".page-title a::text").get(),
                 "nota" : teaser.css(".score::text").get(),
+                "link_pagina" : teaser.css(".page-title a::attr(href)").get(),
         }
 #response.css(".pager__item .button::attr(href)").get() link para abrir mais reviews
