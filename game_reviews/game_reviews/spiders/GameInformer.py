63347525e594c4cf3a69e433bfa465822c4357b2
Bruno Rodrigues <bf17669@gmail.com>
2021-11-23 09:30:10 -0300

    nova spider

--- game_reviews/game_reviews/spiders/GameInformer.py
+++ game_reviews/game_reviews/spiders/GameInformer.py
@@ -7,10 +7,15 @@ class GameinformerSpider(scrapy.Spider):
     start_urls = ['http://gameinformer.com/reviews']
 
     def parse(self, response):
-        for teaser in response.css(".teaser"):
-            yield{
-                "titulo" : teaser.css(".page-title a::text").get(),
-                "nota" : teaser.css(".score::text").get(),
-                "link_pagina" : teaser.css(".page-title a::attr(href)").get(),
+        for link in response.css(".page-title a::attr(href)"):
+            link = "http://gameinformer.com"+link.get()
+            yield response.follow(link, callback= self.parse_game)
+
+    def parse_game(self, response): 
+        yield {
+            "titulo":response.css(".gi5-dynamic-token-fieldnode-game-as-title .page-title::text").get(),
+            "nota":response.css(".review-summary-score::text").get(),
+            "publisher":response.css(".game-details-publisher::text").get(),
+            "desenvolvedora":response.css(".game-details-developer::text").get(),
+            "lancamento":response.css(".datetime::text").get(),
         }
-#response.css(".pager__item .button::attr(href)").get() link para abrir mais reviews
